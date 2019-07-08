051a5f8329b603733bef90612a304d10d665c0ab
Chris Hurst <chris.hurst@stratusgrid.com>
2019-07-08 17:37:24 -0400

    adjusted inputs I know will be needed initially

--- -inputs.tf
+++ -inputs.tf
@@ -1,16 +1,16 @@
 ### Commented out name_prefix and name_suffix since there can only be one config stream and you don't see the name anywhere
 ### When you have names and the name changes, it requires manual intervention.
 
-# variable "name_prefix" {
-#   description = "String to prefix on object names"
-#   type = "string"
-# }
+variable "name_prefix" {
+  description = "String to prefix on object names"
+  type = "string"
+}
 
-# variable "name_suffix" {
-#   description = "String to append to object names. This is optional, so start with dash if using"
-#   type = "string"
-#   default = ""
-# }
+variable "name_suffix" {
+  description = "String to append to object names. This is optional, so start with dash if using"
+  type = "string"
+  default = ""
+}
 
 variable "input_tags" {
   description = "Map of tags to apply to resources"
@@ -21,25 +21,25 @@ variable "input_tags" {
   }
 }
 
-variable "include_global_resource_rules" {
-  description = "True/False to add global resource rules to Config. Default is false"
-  type = "string"
-  default = false
-}
+# variable "include_global_resource_rules" {
+#   description = "True/False to add global resource rules to Config. Default is false"
+#   type = "string"
+#   default = false
+# }
 
-variable "required_tags_enabled" {
-  description = "True/False to add RequiredTags to Config. Default is false"
-  type = "string"
-  default = false
-}
+# variable "required_tags_enabled" {
+#   description = "True/False to add RequiredTags to Config. Default is false"
+#   type = "string"
+#   default = false
+# }
 
-variable "required_tags" {
-  description = "Map of tag keys, and optionally values, that are required."
-  type = "map"
-  default = {}
-}
+# variable "required_tags" {
+#   description = "Map of tag keys, and optionally values, that are required."
+#   type = "map"
+#   default = {}
+# }
 
-variable "source_recorder" {
-  description = "The AWS Config Recorder for this region. This is used to set the correct application order/dependency chain."
-  type = "string"
-}
+# variable "source_recorder" {
+#   description = "The AWS Config Recorder for this region. This is used to set the correct application order/dependency chain."
+#   type = "string"
+# }
