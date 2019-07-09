3846c50acb3162e989f338f9bf4a60fbde6d66fc
Chris Hurst <chris.hurst@stratusgrid.com>
2019-07-09 16:49:08 -0400

    updated readme and removed old comments

--- ec2-instance-profile.tf
+++ ec2-instance-profile.tf
@@ -1,16 +1,3 @@
-#This will create a role and instance profile for default use on EC2 instance
-#This includes permissions for the following services
-# - SSM
-# - Cloudwatch Agent with Logging
-
-#S3 buckets may need added, but it is usually only with VPC endpoint, not for things without internet access unless you need additional bucket access.
-
-#Give ability to merge a policy that is fed in as variable.
-
-# Add default S3 in? - https://docs.aws.amazon.com/systems-manager/latest/userguide/setup-create-vpc.html
-
-#Break into separate flags for SSM, CloudWatch Agent, Domain Services, and others? - https://docs.aws.amazon.com/systems-manager/latest/userguide/setup-instance-profile.html
-
 resource "aws_iam_role" "ec2_instance_profile" {
   name               = "${var.instance_profile_name}-role"
   assume_role_policy = <<EOF
