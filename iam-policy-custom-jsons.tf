24be18faa0370d724038997c413504be121a89ef
Chris Hurst <chris.hurst@stratusgrid.com>
2019-07-30 14:41:42 -0400

    more terraform 12 changes

--- iam-policy-custom-jsons.tf
+++ iam-policy-custom-jsons.tf
@@ -2,12 +2,12 @@ resource "aws_iam_policy" "custom" {
   count  = length(var.custom_policy_jsons)
   name   = "${var.instance_profile_name}-custom-policy-${count.index}"
   path   = "/"
-  policy = element(var.custom_policy_jsons[count.index])
+  policy = element(var.custom_policy_jsons, count.index)
 }
 
 resource "aws_iam_role_policy_attachment" "custom" {
   count      = length(var.custom_policy_jsons)
   role       = aws_iam_role.ec2_instance_profile.name
-  policy_arn = element(aws_iam_policy.custom.[count.index].arn)
+  policy_arn = element(aws_iam_policy.custom.*.arn, count.index)
 }
 
