cf409d0c2da765aca7427545e9c3da9398b279a5
Dick Childress <richard.childress@stratusgrid.com>
2021-08-11 17:29:01 -0400

    Added instance profile ARN to outputs.

--- -outputs.tf
+++ -outputs.tf
@@ -3,3 +3,8 @@ output "instance_profile_id" {
   value       = aws_iam_instance_profile.ec2_instance_profile.id
 }
 
+output "instance_profile_arn" {
+  description = "ARN of Instance Profile used to reference the created objects in IAM policies"
+  value       = aws_iam_instance_profile.ec2_instance_profile.arn
+}
+
