5ceba680ed875cf88ae08732db13ccb8c09e9d7a
Pazyljon Tursun <pazyljon.tursun@cbm-fvfhl3vvq05n.mynetworksettings.com>
2022-06-15 21:16:16 -0400

    remove inputs

--- -outputs.tf
+++
@@ -1,5 +0,0 @@
-output "instance_profile_id" {
-  description = "ID of Instance Profile used to reference the created objects in aws_instance resources"
-  value       = aws_iam_instance_profile.ec2_instance_profile.id
-}
-
