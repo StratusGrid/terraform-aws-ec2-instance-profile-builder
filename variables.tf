bd22c080baf1cc6e44f2c7a8961ba251425c111e
Pazyljon Tursun <pazyljon.tursun@cbm-fvfhl3vvq05n.mynetworksettings.com>
2022-06-15 20:13:13 -0400

    add var file

---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "name" {
+  description = "name to prepend to all resource names within module"
+  type        = string
+}
+
+### Commented out name_prefix and name_suffix since there can only be one config stream and you don't see the name anywhere
+### When you have names and the name changes, it requires manual intervention.
+
+variable "instance_profile_name" {
+  description = "Unique string name of instance profile to be created. Also prepends supporting resource names"
+  type        = string
+}
+
+variable "input_tags" {
+  description = "Map of tags to apply to resources"
+  type        = map(string)
+  default = {
+    Developer   = "StratusGrid"
+    Provisioner = "Terraform"
+  }
+}
+
+variable "ssm_policy" {
+  description = "True/False to add ssm policy permissions to the IAM Role for the Instance Profile"
+  type        = string
+  default     = true
+}
+
+variable "cloudwatch_agent_policy" {
+  description = "True/False to add cloudwatch agent policy permissions to the IAM Role for the Instance Profile"
+  type        = string
+  default     = true
+}
+
+variable "custom_policy_jsons" {
+  description = "List of JSON strings of custom policies to be attached to the ec2 instance profile iam role"
+  type        = list(string)
+  default     = []
+}
+
+variable "instance_operating_system" {
+  description = "EC2 Instance Operating System Type"
+  type        = string
+  validation {
+    condition     = contains(["linux", "windows"], var.instance_operating_system)
+    error_message = "Please enter the correct instance operating system type"
+  }
+}
