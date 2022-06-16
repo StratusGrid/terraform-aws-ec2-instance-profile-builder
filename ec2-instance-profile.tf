eab95d06372ce37546c6feda93c5567f66c1a63a
Pazyljon Tursun <pazyljon.tursun@cbm-fvfhl3vvq05n.mynetworksettings.com>
2022-06-15 21:15:51 -0400

    update the condition

--- ec2-instance-profile.tf
+++ ec2-instance-profile.tf
@@ -16,12 +16,41 @@ resource "aws_iam_role" "ec2_instance_profile" {
 }
 EOF
 
+  tags = var.input_tags
+}
+
+resource "aws_iam_role" "vss_instance_profile" {
+  name               = "${var.instance_profile_name}-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "ec2:CreateTags",
+      "Resource": [
+          "arn:aws:ec2:*::snapshot/*",
+          "arn:aws:ec2:*::image/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+          "ec2:DescribeInstances",
+          "ec2:CreateSnapshot",
+          "ec2:CreateImage",
+          "ec2:DescribeImages"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 
   tags = var.input_tags
 }
 
-resource "aws_iam_instance_profile" "linux_instance_profile" {
-  name = var.instance_profile_name
-  role = aws_iam_role.ec2_instance_profile.name
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "${var.instance_operating_system}-${var.instance_profile_name}"
+  role = var.instance_operating_system == "linux" ? aws_iam_role.ec2_instance_profile.name : aws_iam_role.vss_instance_profile.name
 }
-
